be13e697eeb4e024a8f73e79d4b005b94937cffc
Gatozaurio <tavoraromero@gmail.com>
2018-10-28 18:20:34 +0100

    Clase usuario

--- clases.js
+++ clases.js
@@ -8,8 +8,8 @@ class Concierto{
     }
 
     // Setters
-    set concierto(concierto){
-        this._concierto = concierto;
+    set grupo(grupo){
+        this._grupo = grupo;
     }
     set precio(precio){
         this._precio = precio;
@@ -33,5 +33,43 @@ class Concierto{
     }
     get ubicacion(){
         return this._ubicacion;
+    }
+}
+
+class Usuario{
+    // Constructor
+    constructor(nombreUsuario, contraseña, correo, nombreReal){
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.correo = correo;
+        this.nombreReal = nombreReal;
+    }
+
+    // Setters
+    set nombreUsuario(nombreUsuario){
+        this._nombreUsuario = nombreUsuario;
+    }
+    set contraseña(contraseña){
+        this._contraseña = contraseña;
+    }
+    set correo(correo){
+        this._correo = correo;
+    }
+    set nombreReal(nombreReal){
+        this._nombreReal = nombreReal;
+    }
+
+    // Getters
+    get nombreUsuario(){
+        return this._nombreUsuario;
+    }
+    get contraseña(){
+        return this._contraseña;
+    }
+    get correo(){
+        return this._correo;
+    }
+    get nombreReal(){
+        return this._nombreReal;
     }
 }
